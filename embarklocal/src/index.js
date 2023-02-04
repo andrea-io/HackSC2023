77898f8508d341edd09208ce3c678269ef8d327f
David Mallari <dmallari@usc.edu>
2023-02-04 15:09:54 -0800

    quick tutorial

--- embarklocal/src/index.js
+++ embarklocal/src/index.js
@@ -4,13 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+class LandingPage extends React.Component{
+  render() {
+    return(
+      <h1>hello world</h1>
+    )
+
+    
+  }
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <LandingPage/>
     <App />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
