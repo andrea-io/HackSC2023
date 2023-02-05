269c17ae2047182e29c232c72b7281602c8cee05
Andrea Gonzalez <andreag4@usc.edu>
2023-02-05 00:08:06 -0800

    JSX

--- embarklocal/src/App.js
+++ embarklocal/src/App.js
@@ -1,17 +1,17 @@
 import SearchPage from './searchPage.js';
 import LandingPage from './landingPage.js';
 import React from "react";
-import {BrowserRouter, Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path = "/" element={<LandingPage/>} />
-        <Route exact path = "/SearchPage" element={<SearchPage/>} />
-      </Routes>
-    </BrowserRouter>
-  );
+function App() {
+  <>
+    <Router>
+        <Routes>
+          <Route exact path = "/" component={LandingPage} />
+          <Route exact path = "/SearchPage" component={SearchPage} />
+        </Routes>
+      </Router>
+  </>
 };
 
 export default App;
