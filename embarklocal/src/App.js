c57ee6e9c700ce6afef892168cee81abe9386abd
Andrea Gonzalez <andreag4@usc.edu>
2023-02-04 23:38:28 -0800

    router

--- embarklocal/src/App.js
+++ embarklocal/src/App.js
@@ -1,16 +1,16 @@
 import SearchPage from './searchPage.js';
 import LandingPage from './landingPage.js';
 import React from "react";
-import { BrowserRouter as BrowserRouter, Router, Switch, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Router, Route, Routes} from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route exact path='/' element={<SearchPage/>} />
-        <Route path = '/LandingPage' element={<LandingPage/>} />
+        <Route exact path='/' element={<LandingPage/>} />
+        <Route path = '/SearchPage' element={<SearchPage/>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
