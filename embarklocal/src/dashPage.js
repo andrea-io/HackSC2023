4824ad20b65081d3b08242b2bd7699b23a3cfa73
Nicole Concepcion <nconcepc@usc.edu>
2023-02-05 03:01:19 -0800

    dash

---
+++ embarklocal/src/dashPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './profilePage.css';
+
+const DashPage = () => {
+    return(
+        <div class = "parent">
+            <h1>Welcome Ana!</h1>
+            <div id = "search">
+                <h2>Where do you want to go?</h2>
+                <div class = "search-bar-container">
+                    <form action = "" class = "search-bar">
+                    <input type = "text" placeholder="Enter your travel location" name = "search" id="idk"></input>
+                    <Link to='/SearchPage'><button><img src = "https://assets.stickpng.com/images/585e4ad1cb11b227491c3391.png" alt = "search-icon"></img></button></Link>
+                    </form>
+                </div>
+            </div> 
+        </div>
+    ) 
+}
+
+export default DashPage;
