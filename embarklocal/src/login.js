f4bad07bea489a5969c03906af87d4f6c1ee617b
Nicolas Dimaano <ndimaano@usc.edu>
2023-02-04 22:20:02 -0800

    made more change to login.js im being marginally useful :3

--- embarklocal/src/login.js
+++ embarklocal/src/login.js
@@ -1,77 +1,86 @@
 import { useRef } from 'react';
 import {db} from './firebase'
 import {collection, addDoc} from 'firebase/firestore'
+import useState from 'react'
 
+const LoginPage = () => {
 
-const LoginPage = () =>{
+const[fName,setfName] = useState('')
+const[lName,setlName] = useState('')
+const[getEmail,setEmail] = useState('')
+const[getBio,setBio] = useState('')
+const[getPassword,setPassword] = useState('')
+const[getLocation,setLocation] = useState('')
+const[getPreference,setPreference] = useState('')
 
-    
-    const addProfile = async (e) => {
-        e.preventDefault()
-        try {
-        await addDoc(collection(db, 'tasks'), {
-            firstName: "", //text
-            lastName: "", //text
-            email: "hgjhjg", // text
-            preference: false, //radio
-            password: "afds", // text 
-            bio: "fdasa", // text 
-            loc: "afsdasd", // dropdown
-            applied: [], 
-        })
-        } catch (err) {
-        alert(err)
-        }
+const addProfile = async (e) => {
+    e.preventDefault()
+    try {
+      await addDoc(collection(db, 'tasks'), {
+        firstName: fName, //text
+        lastName: lName, //text
+        email: getEmail, // text
+        preference: getPreference, //radio
+        password: getPassword, // text 
+        bio: getBio, // text 
+        loc: "afsdasd", // dropdown
+        applied: [], 
+      })
+    } catch (err) {
+      alert(err)
     }
+  }
+  
 
-    return (
-        <div className="App">
-            <form onSubmit={addProfile}>
-                <label for="firstName">First Name</label>
-                <input type= "text" id="firstName" name="firstName" placeholder="Your name..."></input>
+return (
+    <div className="App">
+        <form onSubmit={addProfile}>
+            <label for="firstName">First Name</label>
+            <input type= "text" id="firstName" name="firstName" placeholder="Your name..." onChange={(e) => setfName(e.target.value)} value = {fName} ></input>
 
-                <label for="lastName">Last Name</label>
-                <input type= "text" id="lastName" name="lastName" placeholder="Your last name..."></input>
+            <label for="lastName">Last Name</label>
+            <input type= "text" id="lastName" name="lastName" placeholder="Your last name..."  onChange={(e) => setlName(e.target.value)} value = {lName} ></input>
 
-                <label for="email">Email</label>
-                <input type= "text" id="email" name="email" placeholder="Your email"></input>
+            <label for="email">Email</label>
+            <input type= "text" id="email" name="email" placeholder="Your email"  onChange={(e) => setEmail(e.target.value)} value = {getEmail} ></input>
 
-                <label for="password">Password</label>
-                <input type= "text" id="password" name="password" placeholder="Enter Password"></input>
+            <label for="password">Password</label>
+            <input type= "text" id="password" name="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} value = {getPassword} ></input>
 
-                <label for="bio">Bio</label>
-                <input type= "text" id="bio" name="bio" placeholder="Tell us about yourself"></input>
+            <label for="bio">Bio</label>
+            <input type= "text" id="bio" name="bio" placeholder="Tell us about yourself" onChange={(e) => setBio(e.target.value)} value = {getBio} ></input>
 
-                <label for="preference">Preference</label>
-                <label for="preference">
-                    <input type="radio"
-                        id="perference"
-                        name="preference"
-                        value="1">Host</input>
-                </label>
-                <label for="preference">
-                    <input type="radio"
-                        id="preference"
-                        name="preference"
-                        value="0">Tourist</input>
-                </label>
+            <label for="preference">Preference</label>
+            <label for="preference">
+                <input type="radio"
+                       id="perference"
+                       name="preference"
+                       onChange={(e) => setPreference(true)} >Host</input>
+            </label>
+            <label for="preference">
+                <input type="radio"
+                       id="preference"
+                       name="preference"
+                       onChange={(e) => setPreference(false)} >Tourist</input>
+            </label>
 
 
-                <label for="loc">Location</label>
-                <select id="loc" name="loc">
-                <option value="Las Vegas, Nevada">Las Vegas, Nevada</option>
-                <option value="Los Angeles, California">Los Angeles, California</option>
-                <option value="Madrid, Spain">Madrid, Spain</option>
-                <option value="Melbourne, Australia">Melbourne, Australia</option>
-                <option value="New York City, New York">New York City, New York</option>
-                <option value="Rio de Janeiro, Brazil">Rio de Janeiro, Brazil</option>
-                <option value="Other">Other</option>
-                </select>
+            <label for="loc">Location</label>
+            <select id="loc" name="loc">
+              <option value="Las Vegas, Nevada" onChange={(e) => setLocation(e.target.value)} >Las Vegas, Nevada</option>
+              <option value="Los Angeles, California" onChange={(e) => setLocation(e.target.value)} >Los Angeles, California</option>
+              <option value="Madrid, Spain" onChange={(e) => setLocation(e.target.value)} >Madrid, Spain</option>
+              <option value="Melbourne, Australia" onChange={(e) => setLocation(e.target.value)} >Melbourne, Australia</option>
+              <option value="New York City, New York" onChange={(e) => setLocation(e.target.value)} >New York City, New York</option>
+              <option value="Rio de Janeiro, Brazil" onChange={(e) => setLocation(e.target.value)} >Rio de Janeiro, Brazil</option>
+              <option value="Other" onChange={(e) => setLocation(e.target.value)} >Other</option>
+            </select>
 
-                <button type = "submit">Save</button>
-            </form>
-        </div>
-    );
+            <button type = "submit">Save</button>
+        </form>
+    </div>
+);
+
 
 
 
