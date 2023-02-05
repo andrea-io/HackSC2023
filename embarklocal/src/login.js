236de6823f3dc22a4659aa3098221f7fd1edc9f0
Andrea Gonzalez <andreag4@usc.edu>
2023-02-05 04:04:13 -0800

    nico login and css

--- embarklocal/src/login.js
+++ embarklocal/src/login.js
@@ -1,18 +1,18 @@
 import { useRef } from 'react';
 import {db} from './firebase'
+import './login.css'
 import {collection, addDoc, where} from 'firebase/firestore'
 import useState from 'react'
 import {useEffect, query, onSnapshot, addProfile, Link}  from 'react'
+
 const LoginPage = () => {
-
-
   const[getEmail,setEmail] = useState('')
   const[getPassword,setPassword] = useState('')
   const[profile,setProfile] = useState([])
   const[errmsg,setErrmsg] = useState("")
 
   useEffect(() => {
-    const q = query(collection(db, 'profiles'), where("esmail","==",getEmail))
+    const q = query(collection(db, 'profiles'), where("email","==",getEmail))
     onSnapshot(q, (querySnapshot) => {
       setProfile(querySnapshot.docs.map(doc => ({
         id: doc.id,
@@ -29,7 +29,7 @@ const LoginPage = () => {
     
 
   return (
-      <div className="App">
+      <div class ="app">
           <form onSubmit={addProfile}>
               <label for="email">Email</label>
               <input type= "text" id="email" name="email" placeholder="Your email"  onChange={(e) => setEmail(e.target.value)} value = {getEmail} ></input>
@@ -46,10 +46,6 @@ const LoginPage = () => {
           </script>
       </div>
   );
-
-
-
-
 }
 
 export default LoginPage;
