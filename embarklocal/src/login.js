d4c51aefe2b2bac8e72d48db6f60d760d4ef2d04
David Mallari <dmallari@usc.edu>
2023-02-05 02:59:19 -0800

    login stuff

--- embarklocal/src/login.js
+++ embarklocal/src/login.js
@@ -1,50 +1,47 @@
 import { useRef } from 'react';
 import {db} from './firebase'
-import {collection, addDoc} from 'firebase/firestore'
+import {collection, addDoc, where} from 'firebase/firestore'
 import useState from 'react'
 
 const LoginPage = () => {
 
-const[fName,setfName] = useState('')
-const[lName,setlName] = useState('')
-const[getEmail,setEmail] = useState('')
-const[getBio,setBio] = useState('')
-const[getPassword,setPassword] = useState('')
-const[getLocation,setLocation] = useState('')
-const[getPreference,setPreference] = useState('')
 
-const addProfile = async (e) => {
-    e.preventDefault()
-    try {
-      await addDoc(collection(db, 'profiles'), {
-        firstName: fName, //text
-        lastName: lName, //text
-        email: getEmail, // text
-        preference: getPreference, //true means host
-        password: getPassword, // text 
-        bio: getBio, // text 
-        loc: getLocation, // dropdown
-        applied: [], 
-      })
-    } catch (err) {
-      alert(err)
-    }
-  }
-  
+  const[getEmail,setEmail] = useState('')
+  const[getPassword,setPassword] = useState('')
+  const[profile,setProfile] = useState([])
+  const[errmsg,setErrmsg] = useState("")
 
-return (
-    <div className="App">
-        <form onSubmit={addProfile}>
-            <label for="email">Email</label>
-            <input type= "text" id="email" name="email" placeholder="Your email"  onChange={(e) => setEmail(e.target.value)} value = {getEmail} ></input>
+  useEffect(() => {
+    const q = query(collection(db, 'profiles'), where("email","==",getEmail))
+    onSnapshot(q, (querySnapshot) => {
+      setProfile(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+      if(profile[0].data.password == getPassword){
+        console.log("logged")
+      }
+      else{
+        setErrmsg("Invalid Email/Password!")
+      }
+    })
+  },[])
+    
 
-            <label for="password">Password</label>
-            <input type= "text" id="password" name="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} value = {getPassword} ></input>
+  return (
+      <div className="App">
+          <form onSubmit={addProfile}>
+              <label for="email">Email</label>
+              <input type= "text" id="email" name="email" placeholder="Your email"  onChange={(e) => setEmail(e.target.value)} value = {getEmail} ></input>
 
-            <button type = "submit">Save</button>
-        </form>
-    </div>
-);
+              <label for="password">Password</label>
+              <input type= "text" id="password" name="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} value = {getPassword} ></input>
+
+              <button type = "submit">Save</button>
+          </form>
+          <h1>{errmsg}</h1>
+      </div>
+  );
 
 
 
